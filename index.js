d65ab2aaef2826730df7c427d34af61b943e46a0
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-04-08 20:11:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,82 @@
+function getFirstSelector(selector){
+  
+  return document.querySelector(selector)
+  
+  
+}
+
+function nestedTarget(){
+  
+  return document.getElementById('nested').querySelector('div.target')
+  
+  
+  
+}
+
+function increaseRankBy(n){
+  
+  var list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  
+  for (var i = 0; i<list.length; i++){
+    
+    list[i].innerHTML = (n + parseInt(list[i].innerHTML)).toString()
+    
+  }
+  
+  
+}
+
+
+function deepestChild(){
+  
+ // return document.getElementById('grand-node').querySelector('div div div div div')
+  
+  //return document.getElementById('grand-node').querySelectorAll('div')
+  var nodes = document.getElementById('grand-node').querySelectorAll('div')
+  
+  var current = nodes
+  var next = []
+  
+  
+  while (current.length > 0 ){
+    
+    for(var i = 0; i<current.length; i++){
+      
+      next = current[i].querySelectorAll('div')
+      //console.log(current)
+      
+    }
+   current = next.shift()
+  }
+  
+  return current[0]
+  
+  //return nodes[nodes.length - 1]
+  
+
+ /* var current = nodes
+  var next = []
+  
+  while(Array.isArray(current)){
+      
+      for (let i = 0; i < current.length; i++){
+        
+        next.push(current[i])
+        
+      }
+    current = next.shift()
+    
+    
+  }
+  
+*/    
+    
+    
+    
+  
+  
+  
+  
+}
+
+
